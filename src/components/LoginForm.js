10c75fd6f467133051f23994eacaba3150a8972e
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2023-09-16 01:18:28 +0530

    loginform code added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginForm() {
+    const[formdata,setFormdata]=useState({
+        email:"",password:""
+    })
+    function changeHandeler(event)
+    {
+       
+        setFormdata((prev)=>({...prev,
+        [event.target.name]:event.target.value}))
+    }
   return (
-    <div>
+    <form >
+        <label >
+               <p>
+                Email Address <sup>*</sup>
+                </p>
       
-    </div>
+        <input type="text" required placeholder='Enter your email id' value={FormData.email}
+        onChange={changeHandeler}
+        />
+          </label>
+    </form>
   )
 }
 
