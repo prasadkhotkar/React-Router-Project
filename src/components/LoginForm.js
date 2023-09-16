3e97cdb52c755af3909f7946b86719af66f5c2a5
prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2023-09-16 19:06:12 +0530

    remaining code of form added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import { AiOutlineEye,AiOutlineEyeInvisible} from "react-icons/ai";
 
 function LoginForm() {
     const[formdata,setFormdata]=useState({
         email:"",password:""
     })
+    const[showpassword,setShowpassword]=useState(false);
     function changeHandeler(event)
     {
        
@@ -14,13 +16,32 @@ function LoginForm() {
     <form >
         <label >
                <p>
-                Email Address <sup>*</sup>
+                Email Address<sup>*</sup>
                 </p>
       
         <input type="text" required placeholder='Enter your email id' value={FormData.email}
-        onChange={changeHandeler}
+        onChange={changeHandeler} name="email"
         />
           </label>
+          <label >
+            <p>
+              password<sup>*</sup>
+            </p>
+            <input type={showpassword ?("text"):("password")} required placeholder='Enter password' value={FormData.password}
+        onChange={changeHandeler} name="password"
+        />
+        <span onClick={()=>setShowpassword((prev)=>!prev)}>
+          {showpassword ? (<AiOutlineEyeInvisible/>):(<AiOutlineEye/>)}
+        </span>
+        <Link to="#">
+          <p>
+            Forgot password
+          </p>
+        </Link>
+          </label>
+          <button>
+            Sign In
+          </button>
     </form>
   )
 }
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SignupForm() {
   return (
-    <div>
-      
+    <div >
+           
     </div>
   )
 }
